6eeda62d47494bb95baa44a54117da45363f079d
Tsum <111987546+LyWilson@users.noreply.github.com>
2024-03-20 08:48:38 -0400

    Add files via upload
    first commit

---
+++ login/scriptMDP.js
@@ -0,0 +1,6 @@
+document.getElementById("ResetPassword").addEventListener("click", function() {
+
+    var email = document.getElementById("email").value;
+
+    alert("Un email de réinitialisation du mot de passe a été envoyé à " + email);
+});

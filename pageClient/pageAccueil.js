6c82ab2aa4fd29448add34dc950f198a37578f71
Lazianime <lyandyphok@gmail.com>
2024-04-01 14:48:46 -0500

    bruh jarrive pas a avoir le nav burger quand la fenetre est plus grande

--- pageClient/pageAccueil.js
+++ pageClient/pageAccueil.js
@@ -1,12 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
+function setupNavbar() {
   const burgerMenu = document.querySelector('.navbar-burger');
   const navbarItems = document.getElementById('navbarItems');
 
+  // Ouverture/Fermeture du menu burger
   burgerMenu.addEventListener('click', function() {
     navbarItems.classList.toggle('slide-in');
     navbarItems.classList.toggle('slide-out');
   });
 
+    // Fermeture du menu burger si on clique en dehors
   document.addEventListener('click', function(event) {
     const isClickInside = navbarItems.contains(event.target) || burgerMenu.contains(event.target);
     if (!isClickInside && navbarItems.classList.contains('slide-in')) {
@@ -14,4 +16,9 @@ document.addEventListener("DOMContentLoaded", function() {
       navbarItems.classList.add('slide-out');
     }
   });
+}
+
+// Appel des fonctions au chargement de la page
+document.addEventListener("DOMContentLoaded", function() {
+  setupNavbar();
 });

386615a80f0491f8c793455081b2585675653509
LAP <lyandyphok@gmail.com>
2024-04-03 18:44:37 -0400

    new hierarchy and barber shops list

--- db.js
+++ db.js
@@ -2,35 +2,16 @@ const sql = require('mssql');
 const config = {
   user: 'admin',
   password: 'admin',
-  server: 'localhost',
+  server: 'LAPtop', //LAPtop, DESKTOP-LAP
   database: 'Barbier_v3',
+  port: 1433,
   options: {
-    trustServerCertificate: true,
-    encrypt: true
+    trustedConnection: true,
+    encrypt: false
   }
 };
 
-// Fonction pour obtenir les salons de coiffure de la base de données
-async function getSalonDeCoiffure() {
-  try {
-    await sql.connect(config);
-    const resultat = await sql.query`SELECT nomSalon, adresse, numeroTelephoneSalon, horairesOuverture FROM Salon`;
 
-    return resultat.recordset;
-  } catch (err) {
-    console.error('Error:', err.message);
-  } finally {
-    sql.close();
-  }
-}
-
-// Route pour obtenir les salons de coiffure
-app.get("/getSalonDeCoiffure", async (req, res) => {
-  try {
-    const salons = await getSalonDeCoiffure();
-    res.json(salons);
-  } catch (error) {
-    console.error(error)
-    res.status(500).send("Erreur lors de la récupération des salons de coiffure.");
-  }
-});
+module.exports = {
+  sql, config
+};
--- pageClient/public/rendezVous.js
+++
@@ -1,5 +0,0 @@
-async function getRendezVous() {
-  try {
-    const
-  }
-}

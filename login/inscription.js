12e73a053e2b8a1b495467972cd1013831776b1f
Wilson Ly <e2254323@cmaisonneuve.qc.ca>
2024-04-04 12:25:35 -0400

    inscription

--- login/inscription.js
+++ login/inscription.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const {ConnectionPool} = require("mssql");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs");gi
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 

e60e5bcac799608680de05f42a7c70ea58de07ef
Wilson Ly <e2254323@cmaisonneuve.qc.ca>
2024-04-05 11:13:59 -0400

    navbar

--- commun.js
+++ commun.js
@@ -1,11 +1,10 @@
 // Navigation Bar
 function getNavbar() {
   return `
-  <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+  <nav class="navbar custom-color" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-        <a class="navbar-item" href="index.html">
-            <img src="logo.png" alt="DWAN Coiffure" style="border-radius: 75%; width: auto; height: auto " >
-            <strong>DWAN Coiffure</strong>
+        <a class="navbar-item" href="#">
+            <img src="logo.png" alt="DWAN Coiffure" style="border-radius: 75%; width: auto; height: auto ">
         </a>
         <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
             <span aria-hidden="true"></span>
@@ -22,11 +21,11 @@ function getNavbar() {
         </div>
 
         <div class="navbar-end">
-            <a class="navbar-item" href="profil">Mon Profil</a>
-            <a class="navbar-item" href="rendezVous">Mes Rendez-vous</a>
-            <a class="navbar-item" href="favoris">Mes Favoris</a>
-            <a class="navbar-item" href="connexion">Déconnexion</a>
-            <a class="navbar-item" href="aide">Aide</a>
+            <a class="navbar-item text-white" href="profil">Mon Profil</a>
+            <a class="navbar-item text-white" href="rendezVous">Mes Rendez-vous</a>
+            <a class="navbar-item text-white" href="favoris">Mes Favoris</a>
+            <a class="navbar-item text-white" href="connexion">Déconnexion</a>
+            <a class="navbar-item text-white" href="aide">Aide</a>
         </div>
     </div>
 </nav>

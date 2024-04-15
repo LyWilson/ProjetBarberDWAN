2dc28a6e5d03f422eaa560819cbf51cfd76a356b
DanielTo1231 <e2155899@cmaisonneuve.qc.ca>
2024-04-14 22:09:47 -0400

    moved map in to js and install mapbox dependencies

--- applicationClient/accueil/accueil.js
+++ applicationClient/accueil/accueil.js
@@ -54,7 +54,7 @@ function Auth() {
   }
 }
 
-// Fonction pour filtrer les salons 
+// Fonction pour filtrer les salons
 function filtrerSalons() {
   const searchInput = document.querySelector('.navbar-item input[type="text"]');
   const searchValue = searchInput.value.toLowerCase().trim();
@@ -66,10 +66,85 @@ function filtrerSalons() {
     const salonAddress = salon.querySelector('.content p:first-child').textContent.toLowerCase();
 
     if (salonName.includes(searchValue) || salonAddress.includes(searchValue)) {
-      salon.style.display = "block"; 
+      salon.style.display = "block";
     } else {
-      salon.style.display = "none"; 
+      salon.style.display = "none";
     }
+  });
+}
+
+// Function to initialize and load the map
+function GenereMap() {
+  mapboxgl.accessToken = 'pk.eyJ1IjoiaW5jZHdhbiIsImEiOiJjbHV2ejFiNDYwN3c3MmlvNTBjbXAyY3E4In0.L4smj76jBXbuZNng4D7iTA';
+  const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: [-73.623848, 45.520123], // starting position [lng, lat]
+    zoom: 10 // starting zoom
+  });
+
+  map.on('load', () => {
+    map.addSource('places', {
+      'type': 'geojson',
+      'data': {
+        'type': 'FeatureCollection',
+        'features': [
+          {
+            'type': 'Feature',
+            'properties': {
+              'description': '<strong>M9</strong><p>Dwan office</p>'
+            },
+            'geometry': {
+              'type': 'Point',
+              'coordinates': [-73.553131, 45.550018]
+            }
+          }
+        ]
+      }
+    });
+    // Add a layer showing the places.
+    map.addLayer({
+      'id': 'places',
+      'type': 'circle',
+      'source': 'places',
+      'paint': {
+        'circle-color': '#4264fb',
+        'circle-radius': 6,
+        'circle-stroke-width': 2,
+        'circle-stroke-color': '#ffffff'
+      }
+    });
+
+    // Create a popup, but don't add it to the map yet.
+    const popup = new mapboxgl.Popup({
+      closeButton: false,
+      closeOnClick: false
+    });
+
+    map.on('mouseenter', 'places', (e) => {
+      // Change the cursor style as a UI indicator.
+      map.getCanvas().style.cursor = 'pointer';
+
+      // Copy coordinates array.
+      const coordinates = e.features[0].geometry.coordinates.slice();
+      const description = e.features[0].properties.description;
+
+      // Ensure that if the map is zoomed out such that multiple
+      // copies of the feature are visible, the popup appears
+      // over the copy being pointed to.
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+
+      // Populate the popup and set its coordinates
+      // based on the feature found.
+      popup.setLngLat(coordinates).setHTML(description).addTo(map);
+    });
+
+    map.on('mouseleave', 'places', () => {
+      map.getCanvas().style.cursor = '';
+      popup.remove();
+    });
   });
 }
 
@@ -79,5 +154,6 @@ document.addEventListener("DOMContentLoaded", () => {
   generateFooter();
   loadSalons();
   deconnexion();
+  GenereMap();
   document.addEventListener('input', filtrerSalons)
 });

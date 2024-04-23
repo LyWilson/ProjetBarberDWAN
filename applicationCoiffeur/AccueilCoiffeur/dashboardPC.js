0647ac9daae9fe962ee206cae9f448e403c72753
Wilson Ly <e2254323@cmaisonneuve.qc.ca>
2024-04-23 18:42:48 -0400

    changement fonction dashcoardpc

--- applicationCoiffeur/AccueilCoiffeur/dashboardPC.js
+++ applicationCoiffeur/AccueilCoiffeur/dashboardPC.js
@@ -1,21 +1,5 @@
 import {deconnexion, generateFooter, generateNavBarWithAuth} from "../../commun.js";
 
-document.addEventListener("DOMContentLoaded", async function(event) {
-    event.preventDefault()
-    const token = sessionStorage.getItem('tokenCoiffeur');
-    const info = token => decodeURIComponent(atob(token.split('.')[1].replace('-', '+').replace('_', '/')).split('').map(c => `%${('00' + c.charCodeAt(0).toString(16)).slice(-2)}`).join(''));
-    const email = JSON.parse(info(token)).email;
-    const resultat = await fetch(`/getSalonId?email=${email}`)
-    const salonId = await resultat.json();
-    generateFooter();
-    generateNavBarWithAuth();
-    deconnexion();
-    initializePieChart(salonId);
-    initbarChart();
-    initLineChart()
-});
-
-
 //Pie Chart
 async function initializePieChart(salonId) {
     // Make sure the salonId is valid or exit the function
@@ -178,4 +162,19 @@ function initLineChart() {
     const ctx = document.getElementById('lineChartContainer').getContext('2d');
     const lineChart = new Chart(ctx, config);
 }
+
+document.addEventListener("DOMContentLoaded", async function(event) {
+    event.preventDefault()
+    const token = sessionStorage.getItem('tokenCoiffeur');
+    const info = token => decodeURIComponent(atob(token.split('.')[1].replace('-', '+').replace('_', '/')).split('').map(c => `%${('00' + c.charCodeAt(0).toString(16)).slice(-2)}`).join(''));
+    const email = JSON.parse(info(token)).email;
+    const resultat = await fetch(`/getSalonId?email=${email}`)
+    const salonId = await resultat.json();
+    generateFooter();
+    generateNavBarWithAuth();
+    deconnexion();
+    initializePieChart(salonId);
+    initbarChart();
+    initLineChart()
+});
 

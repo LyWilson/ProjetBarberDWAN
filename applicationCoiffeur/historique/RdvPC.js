b60c10a68584e220895f4cf0632f03db8a293f90
DanielTo1231 <e2155899@cmaisonneuve.qc.ca>
2024-05-07 22:05:21 -0400

    séparer historique et upcoming rdv coiffeur

--- applicationCoiffeur/historique/RdvPC.js
+++ applicationCoiffeur/historique/RdvPC.js
@@ -1,7 +1,5 @@
 import {authCoiffeur, deconnexion, generateFooter, generateNavBarWithAuth} from "../../commun.js";
 
-const salonId = 1;
-
 async function fetchReservationsBySalonId(salonId) {
     try {
         const response = await fetch(`/getReservationsBySalonId?salonId=${salonId}`);
@@ -16,30 +14,45 @@ async function fetchReservationsBySalonId(salonId) {
 }
 
 function updateReservationsSection(reservations) {
-    const historyBox = document.querySelector('.box');
-    historyBox.innerHTML = `
-        <p class="menu-label">Historique des rendez-vous</p>
-        <div class="content">
-            <ul>
-                ${reservations.map(reservation => `
-                    <li>
-                        <p>Rendez-vous ID: ${reservation.reservationId}</p>
-                        <p>Date et heure: ${reservation.dateHeureReservation}</p>
-                        <p>Durée: ${reservation.dureeReservation} minutes</p>
-                        <p>Coiffeur: ${reservation.coiffeurNom} ${reservation.coiffeurPrenom}</p>
-                        <p>Salon: ${reservation.nomSalon}</p>
-                        <p>Adresse: ${reservation.adresse}</p>
-                        <p>Coiffure: ${reservation.nomCoiffure}</p>
-                        <p>Description de la coiffure: ${reservation.descriptionCoiffure}</p>
-                    </li></br>
-                `).join('')}
-            </ul>
-        </div>
+    console.log(reservations);
+    const currentDate = new Date();
+    const upcomingReservations = reservations.filter(reservation => new Date(reservation.dateHeureReservation) > currentDate);
+    const pastReservations = reservations.filter(reservation => new Date(reservation.dateHeureReservation) <= currentDate);
+
+    const upcomingBox = document.getElementById('upcomingBox');
+    const historyBox = document.getElementById('historyBox');
+
+    upcomingBox.innerHTML = generateReservationList(upcomingReservations);
+    historyBox.innerHTML = generateReservationList(pastReservations);
+}
+
+function generateReservationList(reservations) {
+    return `
+        <ul>
+            ${reservations.map(reservation => `
+                <li>
+                    <p><b>Rendez-vous ID:</b> ${reservation.reservationId}</p>
+                    <p><b>Date et heure:</b> ${reservation.dateHeureReservation}</p>
+                    <p><b>Durée:</b> ${reservation.dureeReservation} minutes</p>
+                    <p><b>Coiffeur:</b> ${reservation.coiffeurNom} ${reservation.coiffeurPrenom}</p>
+                    <p><b>Salon:</b> ${reservation.nomSalon}</p>
+                    <p><b>Adresse:</b> ${reservation.adresse}</p>
+                    <p><b>Coiffure:</b> ${reservation.nomCoiffure}</p>
+                    <p><b>Description de la coiffure:</b> ${reservation.descriptionCoiffure}</p>
+                </li></br></br>
+            `).join('')}
+        </ul>
     `;
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async function(event) {
     authCoiffeur();
+    event.preventDefault();
+    const token = sessionStorage.getItem('tokenCoiffeur');
+    const info = token => decodeURIComponent(atob(token.split('.')[1].replace('-', '+').replace('_', '/')).split('').map(c => `%${('00' + c.charCodeAt(0).toString(16)).slice(-2)}`).join(''));
+    const email = JSON.parse(info(token)).email;
+    const resultat = await fetch(`/getSalonId?email=${email}`)
+    const salonId = await resultat.json();
     generateFooter();
     generateNavBarWithAuth()
     deconnexion()

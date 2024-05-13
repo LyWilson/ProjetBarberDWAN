f94a0a94b517c99fa493002b17f0621ade32db63
Wilson Ly <e2254323@cmaisonneuve.qc.ca>
2024-05-13 00:51:59 -0400

    upload image

--- applicationCoiffeur/AppCoiffeurReq.js
+++ applicationCoiffeur/AppCoiffeurReq.js
@@ -6,6 +6,21 @@ const router = express.Router();
 
 const { getReservationsByCoiffeurId, getSalonId, getUserId, getHeuresTravail, getCoiffeurId, updateSponsor, getAvisClientsById} = require('../fonctionDb');
 
+const countImages = (directory) => {
+    return new Promise((resolve, reject) => {
+        fs.readdir(directory, (err, files) => {
+            if (err) {
+                reject(err);
+            } else {
+                const imageFiles = files.filter(file => {
+                    return ['.jpg', '.jpeg', '.png', '.gif'].includes(path.extname(file).toLowerCase());
+                });
+                resolve(imageFiles.length);
+            }
+        });
+    });
+};
+
 // Middleware to create directory if it doesn't exist
 const createDir = (dir) => {
     if (!fs.existsSync(dir)){
@@ -13,25 +28,35 @@ const createDir = (dir) => {
     }
 }
 
-// Configure storage for Multer
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
+    destination: async (req, file, cb) => {
         const { salonId } = req.params;
         const uploadDir = path.join(__dirname, `../Image/salon${salonId}/Portfolio${salonId}`);
         createDir(uploadDir);
+
         cb(null, uploadDir);
     },
-    filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+    filename: async (req, file, cb) => {
+        const { salonId } = req.params;
+        const uploadDir = path.join(__dirname, `../Image/salon${salonId}/Portfolio${salonId}`);
+        const imageCount = await countImages(uploadDir);
+
+        // Construire le nouveau nom de fichier
+        const newFileName = `haircut${imageCount + 1}${path.extname(file.originalname)}`;
+
+        cb(null, newFileName);
     }
 });
+
 
 const upload = multer({ storage: storage });
 
 // Route to handle file upload
 router.post('/upload/:salonId', upload.single('photo'), (req, res) => {
-    console.log(req.params.salonId);
+    const { salonId } = req.params;
+    const fileName = req.file.filename;
+    const filePath = req.file.path;
+    console.log(`Salon ID: ${salonId}, File Name: ${fileName}, File Path: ${filePath}`);
     res.send('File uploaded successfully!');
 });
 

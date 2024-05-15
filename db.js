42677397c5165271c9505785be83662f84dec4fb
Wilson Ly <e2254323@cmaisonneuve.qc.ca>
2024-05-14 22:42:11 -0400

    delete msg code

--- db.js
+++ db.js
@@ -10,41 +10,6 @@ const config = {
   }
 };
 
-/* 
-// Fonction pour tester la connexion à la base de données en fetch les données de la table
-async function fetchDataFromTable(tableName) {
-  try {
-    await sql.connect(config);
-    const request = new sql.Request();
-    request.input('tableName', sql.VarChar, tableName);
-    const result = await request.query(`SELECT * FROM ${tableName}`);
-    return result.recordset;
-  } catch (err) {
-    console.error(`Error fetching data from table ${tableName}:`, err);
-    throw err;
-  } finally {
-    await sql.close();
-  }
-}
-
-// Fonction pour afficher toutes les données dans le terminal
-async function displayAllDataInTerminal() {
-  try {
-    const tables = ['Salon', 'Client', 'Coiffeur', 'Reservation'];
-    console.log('Data from the database:');
-    for (const table of tables) {
-      const data = await fetchDataFromTable(table);
-      console.log(`Table: ${table}`);
-      console.table(data);
-    }
-  } catch (err) {
-    console.error('Error displaying data in terminal:', err);
-  }
-}
-
-displayAllDataInTerminal();
-*/
-
 module.exports = {
   sql, config
 };
